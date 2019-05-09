8fe5672ca7912c2ae63a2c395d6e91a4c8210282
Jenell-w <49175487+Jenell-w@users.noreply.github.com>
2019-05-08 21:55:35 -0600

    Update sort_contacts.py

--- sort_contacts.py
+++ sort_contacts.py
@@ -1,3 +1,10 @@
-def sort_contacts(param):
-    # code here
-    return ""
+def sort_contacts(contact_dict):
+    contact_dictItems = sorted(contact_dict.items())
+    contact_dict_list = list(contact_dictItems)
+    
+    sorted_list = []
+    for key, value in contact_dict_list:
+        sorted_list.append((key, value[0], value[1]))
+    
+    return sorted_list
+    
